bfe640f9f08470b26ac5652971c4f3e3d8aedc90
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-20 19:31:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.map do |item, value|
-  if 
+name_hash.map do |item, amt|
+  amt.sort
+  return item
   end
 end
